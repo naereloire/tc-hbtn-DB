f5005db568ad28f1c8aad41a884b483d5d321e62
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-06-08 19:52:03 -0300

    fix print drivers to show version

--- 0x01/java_jdbc/PrintJDBCDrivers.java
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -9,9 +9,11 @@ import static java.lang.System.out;
 public class PrintJDBCDrivers {
   public static void main(String[] args) {
     Enumeration<Driver> drivers = DriverManager.getDrivers();
-    while (drivers.hasMoreElements())
+    while (drivers.hasMoreElements()) {
+      Driver driver = drivers.nextElement();
+
       out.printf(
-              "Nome: %s %n",
-          drivers.nextElement().getClass().getSimpleName());
+          "Nome: %s Versão: %d", driver.getClass().getName(), driver.getMajorVersion());
+    }
   }
 }
