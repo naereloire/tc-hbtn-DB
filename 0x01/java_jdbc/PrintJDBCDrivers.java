a3ceb6de235afbaa7e4a60dc98f97d0f0f484d34
Naere Loire Guimarães dos Santos <CIANDT\naere@lnb028103spo.ciandt.global>
2022-06-08 16:02:43 -0300

    print drivers

--- 0x01/java_jdbc/PrintJDBCDrivers.java
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -1,7 +1,17 @@
 package java_jdbc;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+import static java.lang.System.out;
+
 public class PrintJDBCDrivers {
   public static void main(String[] args) {
-    System.out.println();
+    Enumeration<Driver> drivers = DriverManager.getDrivers();
+    while (drivers.hasMoreElements())
+      out.printf(
+              "Nome: %s %n",
+          drivers.nextElement().getClass().getSimpleName());
   }
 }
